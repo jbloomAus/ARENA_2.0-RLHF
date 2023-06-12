cfb59af5312747efca40dc398b0b1bd86a7da10f
atagade <atagade19@gmail.com>
2023-06-12 14:48:58 +0100

    Added neutral review feedback

--- ppo_sentiments.py
+++ ppo_sentiments.py
@@ -2,6 +2,7 @@
 import json
 import os
 import sys
+import math
 from typing import List
 
 import torch
@@ -81,6 +82,52 @@ def reward_fn(samples: List[str], **kwargs) -> List[float]:
 def get_positive_score(scores):
     "Extract value associated with a positive sentiment from pipeline's output"
     return dict(map(lambda x: tuple(x.values()), scores))["POSITIVE"]
+
+
+def main(hparams={}):
+    # Merge sweep config with default config if given
+    config = TRLConfig.update(default_ppo_config().to_dict(), hparams)
+
+    if torch.cuda.is_available():
+        device = int(os.environ.get("LOCAL_RANK", 0))
+    else:
+        device = -1
+
+    sentiment_fn = pipeline(
+        "sentiment-analysis",
+        "lvwerra/distilbert-imdb",
+        top_k=2,
+        truncation=True,
+        batch_size=256,
+        device=device,
+    )
+
+    def reward_fn(samples: List[str], **kwargs) -> List[float]:
+        sentiments = list(map(get_positive_score, sentiment_fn(samples)))
+        return sentiments
+
+    # Take few words off of movies reviews as prompts
+    imdb = load_dataset("imdb", split="train+test")
+    prompts = [" ".join(review.split()[:4]) for review in imdb["text"]]
+
+    train(
+        model_path='lvwerra/gpt2-imdb',
+        reward_fn=reward_fn,
+        prompts=prompts,
+        eval_prompts=["I aimed my gun at the man and pulled the trigger"] * 256,
+        config=config,
+    )
+
+
+if __name__ == "__main__":
+    hparams = {} 
+    main(hparams)
+# %%
+
+# Neutral Reviews
+def get_positive_score(scores):
+    "Extract value associated with a positive sentiment from pipeline's output"
+    return 1 / (abs(dict(map(lambda x: tuple(x.values()), scores))["POSITIVE"] - dict(map(lambda x: tuple(x.values()), scores))["NEGATIVE"]) + 0.0001)
 
 
 def main(hparams={}):
